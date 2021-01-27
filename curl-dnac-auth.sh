f52d255cd3e68f7d19c3bc15598a0b4c9f24b723
blackbeard094 <luarroyo.cisco@gmail.com>
2021-01-27 09:17:18 -0500

    Push all files

---
+++ curl-dnac-auth.sh
@@ -0,0 +1,13 @@
+Curl -L -X POST \
+-u 'devnetuser:Cisco123!' \
+-H  'Content-Type: application/json' \
+-H 'Content-Length: 0' \
+https://sandboxdnac.cisco.com/dna/system/api/v1/auth/token 
+
+
+#curl -L --request POST \
+#--url https://sandboxdnac.cisco.com/dna/system/api/v1/auth/token \
+#--header 'Content-Type: application/json' \
+#--header 'Authorization: Basic ZGV2bmV0dXNlcjpDaXNjbzEyMyE=' \
+#--header 'Content-Length: 0' \
+#--header 'Accept: application/json'
---
+++ curl-lab-auth.sh
@@ -0,0 +1,6 @@
+curl -L -k --request POST \
+-u 'admin:1qaz2wsx!QAZ@WSX' \
+-H 'Content-Type: application/json' \
+-H 'Content-Length: 0' \
+-H 'Accept: application/json' \
+https://10.81.237.254/dna/system/api/v1/auth/token
